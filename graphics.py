1f95271b23143826c75ef8ab490efe358ab59044
kbresser <me@kevinbresser.com>
2025-05-14 14:42:00 -0400

    cells

--- graphics.py
+++ graphics.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
@@ -8,7 +9,6 @@ class Window:
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-  
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -20,22 +20,28 @@ class Window:
             self.redraw()
         print("window closed...")
 
-    def draw_line(self, line, fill_color):
+    def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
-    
+
     def close(self):
         self.__running = False
+
 
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+
 class Line:
-    def __init__(self, p1, p2):
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
         self.p1 = p1
         self.p2 = p2
-    
+
     def draw(self, canvas, fill_color="black"):
         canvas.create_line(
             self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
