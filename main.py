2bd2a92a3a7d95bbb28014f5155288c003f8d508
kbresser <me@kevinbresser.com>
2025-05-14 14:04:03 -0400

    window and lines

---
+++ main.py
@@ -0,0 +1,10 @@
+from classes import Window, Point, Line
+
+def main():
+    win = Window(800, 600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
+    win.wait_for_close()
+
+
+main()
