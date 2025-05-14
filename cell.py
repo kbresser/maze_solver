05a1171546ab0132f00d49bd053da242b53f7470
kbresser <me@kevinbresser.com>
2025-05-14 15:39:50 -0400

    path line

--- cell.py
+++ cell.py
@@ -1,6 +1,5 @@
 from graphics import Line, Point
 
-
 class Cell:
     def __init__(self, win):
         self.has_left_wall = True
@@ -14,6 +13,8 @@ class Cell:
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -30,3 +31,13 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        color = "gray" if undo else "red"
+        x1 = (self._x1 + self._x2) / 2
+        y1 = (self._y1 + self._y2) / 2
+        x2 = (to_cell._x1 + to_cell._x2) / 2
+        y2 = (to_cell._y1 + to_cell._y2) / 2
+
+        line = Line(Point(x1, y1), Point(x2, y2))
+        self._win.draw_line(line, color)
